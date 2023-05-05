1a93780c6ba676811faef40ed486de7ab2ed6bd7
danarbastian7 <danarbastian7@gmail.com>
2023-05-06 01:13:08 +0900

    second commit

---
+++ LearningPy1.py
@@ -0,0 +1,40 @@
+# print("Hello world!")
+
+# print("Hello world\nHello world")
+
+# print("Hello" + "angela")
+# print("Hello" + " " + "angela")
+
+# Error code writing
+# print(Day-1 String manipulation) -- not has a string
+
+    # print("Hello world") -- indent errr
+
+######### Input function ##########
+# input("what is your name?")
+# print("Hello " + input("What is your name?"))
+
+# Length function
+# print(len(input("What is your name? ")))
+
+############# Variabel ############
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+# a = input("a:")
+# b = input("b:")
+
+# print("a = " + b)
+# print("b = " + a )
+
+####### Variable naming #######
+# user_name = input("What is your name? ")
+# length_name = len(user_name)
+# print(length_name)
+
+##### DAY 1 Project Name Generator #####
+
+city_name = input("What's the name of the city you grew up in?\n ")
+pet_name = input("What's your pet's name?\n ")
+print("your band name could be " + (city_name.capitalize()) + " " + pet_name.capitalize())
