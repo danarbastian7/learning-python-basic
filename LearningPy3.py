aedba627f7469c4f0e5066ae045dd3bf006beb7e
danarbastian7 <danarbastian7@gmail.com>
2023-05-09 20:11:28 +0900

    day3 first

---
+++ LearningPy3.py
@@ -0,0 +1,51 @@
+#### DAY 3 (CONDITIONAL STATEMENTS, LOGICAL OPERATORS, CODE BLOCKS, ADN SCOPE)
+
+# ============================
+# print("Welcome to the rollercoaster!")
+
+# height = int(input("What is your height in cm? "))
+
+# if height > 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# ======================
+# ODD OR EVEN NUMBER
+# print("Welcome to Odd or Even Number!")
+# number_input = int(input("Please input a number: "))
+
+# if (number_input % 2) == 1:
+#     print("This is an Odd Number")
+# else:
+#     print("This is an Even Number")
+
+# =============================
+# IF AND ELIF CONDITIONS
+
+# print("Welcome to the rollercoaster!")
+
+# height = int(input("What is your height in cm? "))
+
+# if height > 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age > 18:
+#         print("Please pay $12")
+#     elif age <= 18 and age >= 12:
+#         print("Please pay $7")
+#     elif age < 12:
+#         print("Please pay $5")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# ====================================
+
+input_height = input("Enter your height in m: ")
+input_weight = (input("Enter your weight in kg: "))
+height = float((input_height))
+weight = float((input_weight))
+
+
+# print("Here is your BMI:", +  round(weight/(height ** 2), 0))
+
