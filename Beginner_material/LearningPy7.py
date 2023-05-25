8fae7e839ad81cf17681d71914449031373370e3
danarbastian7 <danarbastian7@gmail.com>
2023-05-25 17:04:14 +0900

    Hangman Project #3

--- Beginner_material/LearningPy7.py
+++ Beginner_material/LearningPy7.py
@@ -1,6 +1,7 @@
 # HANGMAN PROJECT
 
 import random
+from HangmanLogo import logo
 word_list = ["Albania", "Andorra", "Argentina", "Armenia", "Australia",
              "Austria", "Belarus", "Belgium", "Bolivia", "Botswana",
              "Brazil", "Bulgaria", "Cambodia", "Cameroon", "Canada",
@@ -71,21 +72,30 @@ stages = ['''
       |
 =========
 ''']
+
 lives = len(stages) - 1
 answer = False
 
 
-count = -1
+count = 0
 word_choose = list(random.choice(word_list).lower())
-
 word_guess = (["_"] * len(word_choose))
-print(word_guess)
+previous_choose = ""
 
+print(f"{logo}\nPssst, the solution is thriftless")
+print(' '.join(map(str, word_guess)))
 
 while "_" in word_guess:
 
-    guess_input = input("What is your guess? ").lower()
-
+    guess_input = input("What is your guess?\n").lower()
+    count += 1
+    # print(count)
+    if count > 1:
+        if guess_input in previous_choose:
+            print("Input must be different from the previous input")
+            lives += 1
+    # print(previous_choose)
+    previous_choose += guess_input
     for i in range(len(word_choose)):
         if guess_input == word_choose[i]:
             word_guess[i] = guess_input
@@ -93,19 +103,26 @@ while "_" in word_guess:
 
     if not answer:
         lives -= 1
+        print(
+            f"You guess {guess_input}, that's not in the word. You lose a life.")
         if lives == 0:
+
             print("You Lose")
-            print(f"The correct answer is{word_choose}")
+            print(f"The correct answer is {''.join(map(str, word_choose))}")
             print(stages[lives])
             break
     print(stages[lives])
     if (word_choose == word_guess):
         print("You win")
-        print(f"Your word is{word_choose}")
+        print(f"Your word is {''.join(map(str, word_choose))}")
         print(stages[lives])
         break
     answer = False
+    print(' '.join(map(str, word_guess)))
 
-    # print(answer)
-    print(word_guess)
-    # print(word_choose)
+
+# while True:
+#     if guess_input in previous_choose:
+#         print("Input must be different from the previous input")
+# print(answer)
+# print(word_choose)
