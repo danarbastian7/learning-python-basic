91ab45d72a12eb31b54c6fa2eb5d5641f63612b3
danarbastian7 <danarbastian7@gmail.com>
2023-06-15 13:45:52 +0900

    OOP #1

---
+++ Beginner_material/LearningPy16.py
@@ -0,0 +1,20 @@
+# OBJECT ORIENTED PROGRAMMING
+
+# car = CarBluePrint()
+# car = object and CarBluePrint = class
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("chartreuse3")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
+
+
+table = PrettyTable()
+print(table)
