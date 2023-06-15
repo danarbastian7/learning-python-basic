470e931046e4b7a5cf6cd04eb8460c498a72e790
danarbastian7 <danarbastian7@gmail.com>
2023-06-15 14:59:42 +0900

    Coffe Machine OOP #1

--- Beginner_material/LearningPy16.py
+++ Beginner_material/LearningPy16.py
@@ -20,4 +20,6 @@ table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 
+table.align = "l"
+
 print(table)
