0861757e72dc284ca411a1a955056585369467b9
danarbastian7 <danarbastian7@gmail.com>
2023-07-13 10:57:01 +0900

    oop coffee-machine

---
+++ Beginner_material/LearningPy18-turtle-graphics/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+
+timy_the_turtle = Turtle()
+timy_the_turtle.shape("turtle")
+timy_the_turtle.color("red")
+timy_the_turtle.forward(100)
+timy_the_turtle.right(90)
+timy_the_turtle.forward(100)
+timy_the_turtle.right(90)
+timy_the_turtle.forward(100)
+timy_the_turtle.right(90)
+timy_the_turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
